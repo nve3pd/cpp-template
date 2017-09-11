33c346ffac7ea34fde78a7eae24d0728df5782cc
nve3pd <uchihara1229@gmail.com>
2017-09-12 00:50:22 +0900

    support C++11

--- cpp.cpp
+++ cpp.cpp
@@ -10,15 +10,15 @@ struct Fast {Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
 #define rep(i, n) for(int i = 0; i < (n); i++)
 #define RREP(i, n) for(int i = (n) - 1; i >= 0; i--)
 #define rrep(i, n) for(int i = (n) - 1; i >= 0; i--)
+#define each(i, ctn) for (auto &&i : (ctn))
  
 #define all(i) (i).begin(),(i).end()
 #define rall(i) (i).begin(),(i).end(),greater<int>()
 #define CLEAR(ary, i) memset((ary), (i), sizeof(ary))
-#define PB push_back
+#define debug(x) cerr << (x) << " (L:" << __LINE__ << ")" << '\n'
+#define eb emplace_back
 #define pb push_back
 #define mp make_pair
-#define FST first
-#define SEC second
 #define fst first
 #define sec second
 
@@ -26,20 +26,18 @@ struct Fast {Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
 
 const int INF = 999999999;
 const int MOD = 1e9 + 7;
+const double EPS = 1e9 + 7;
 const double PI = acos(-1);
 const int dx[] = {0, 1, 0, -1}, dy[] = {1, 0, -1, 0};
 
 inline int in(){int x;std::cin>>x;return x;}
 
-typedef long long ll;
-typedef pair<int, int> pint;
-typedef vector<int> vi;
-typedef vector<vector<int> > vii;
-typedef vector<long long> llv;
-typedef vector<pint> vpint;
-typedef queue<int> qint;
-typedef stack<int> si;
-typedef deque<int> dint;
+using lint = long long;
+using pint = pair<int, int>;
+using vi = vector<int>;
+using vii = vector<vector<int> >;
+using vl = vector<long long>;
+using vp = vector<pint>;
 
 template <typename T> void print(T x) { std::cout << x << '\n'; }
 template<typename A, size_t N, typename T>
