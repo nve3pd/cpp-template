f4ff0236a9bdafa9197f101811eeb865dd3c4272
mitohato <mitohato14@gmail.com>
2017-10-11 22:04:43 +0900

    Change indent 4 from 4

--- java.java
+++ java.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 class Main {
-  public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-  }
+    }
 }
